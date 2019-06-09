a898ae08385b2a1ea28f2c603a590c161ff741e3
JVSC <joaovictor.clementino@hotmail.com>
2019-06-09 20:02:46 -0300

    fix server port

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import main
+import os
 
-
+_port = int(os.environ.get('PORT', 5000))
 app = Flask(__name__)
 CORS(app)
 @app.route('/solver', methods=['POST'])
@@ -15,4 +16,4 @@ def solver():
 
     return jsonify({'exp': string})
 
-app.run()
+app.run(host='0.0.0.0', port=_port)
