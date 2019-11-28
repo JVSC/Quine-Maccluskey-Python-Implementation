8b5086737768880fb78bdcebbd138b37344952ae
testuser <joaovictor.clementino@gmail.com>
2019-11-27 21:13:04 -0300

    improve model

--- fkNews.py
+++ fkNews.py
@@ -2,8 +2,18 @@ from textblob import TextBlob
 from textblob.classifiers import NaiveBayesClassifier 
 import pandas as pd
 
-news = pd.read_csv('./samples/news.csv', sep=';', header=None)
-clf = NaiveBayesClassifier(news.values, format="csv")
+def panda_parser_train():
+    # id,title,author,text,label
+    train = []
+    n = pd.read_csv('./samples/train.csv', sep=',', header = None)
+    for i in n.values[0:100]:
+        tple = (i[3], i[4])
+        train.append(tple)
+
+    return train 
+
+news = panda_parser_train()
+clf = NaiveBayesClassifier(news, format="csv")
 
 class Classifier:
     def __init__(self, headline):
@@ -11,6 +21,12 @@ class Classifier:
     def classify(self):
         res = clf.prob_classify(self.headline)
         return {
-            'fake_news': res.prob("fake_news"),
-            'verdadeiro': res.prob("verdadeiro")
+            'fake_news': res.prob(0),
+            'verdadeiro': res.prob(1)
         }
+
+
+#n = pd.read_csv('./samples/test.csv', sep=',', header = None)
+#for i in n.values[0:100]:
+#    print(Classifier(i[3]).classify())    
+        
